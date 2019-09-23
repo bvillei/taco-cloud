98176fd2dd9f157337f214e6791e4950a582e3b6
villei <balintvillei@gmail.com>
2019-09-23 15:13:38 +0200

    Working with view controllers

--- src/test/java/tacos/HomeControllerTest.java
+++ src/test/java/tacos/HomeControllerTest.java
@@ -13,10 +13,8 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-import tacos.web.HomeController;
-
 @RunWith(SpringRunner.class)
-@WebMvcTest(HomeController.class)
+@WebMvcTest
 public class HomeControllerTest {
 
 	@Autowired
@@ -27,7 +25,6 @@ public class HomeControllerTest {
 		mockMvc.perform(get("/"))
 		.andExpect(status().isOk())
 		.andExpect(view().name("home"))
-		.andExpect(content().string(
-				containsString("Welcome to...")));
+		.andExpect(content().string(containsString("Welcome to...")));
 	}
 }
