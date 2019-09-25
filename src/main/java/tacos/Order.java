b800de25caddfbce44cb04e0100bd267fed62da8
Balint <csernozjom1@gmail.com>
2019-09-25 18:03:29 +0200

    Add new fields

--- src/main/java/tacos/Order.java
+++ src/main/java/tacos/Order.java
@@ -1,5 +1,7 @@
 package tacos;
 
+import java.util.Date;
+
 import javax.validation.constraints.Digits;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
@@ -10,6 +12,11 @@ import lombok.Data;
 
 @Data
 public class Order {
+
+	private Long id;
+
+	private Date placedAt;
+
 	@NotBlank(message="Name is required")
 	private String name;
 
--- src/main/java/tacos/Taco.java
+++ src/main/java/tacos/Taco.java
@@ -1,5 +1,6 @@
 package tacos;
 
+import java.util.Date;
 import java.util.List;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
@@ -7,9 +8,15 @@ import lombok.Data;
 
 @Data
 public class Taco {
+	
+	private Long id;
+	
+	private Date createdAt;
+
 	@NotNull
 	@Size(min=5, message="Name must be at least 5 characters long")
 	private String name;
+	
 	@Size(min=1, message="You must choose at least 1 ingredient")
 	private List<String> ingredients;
 }
