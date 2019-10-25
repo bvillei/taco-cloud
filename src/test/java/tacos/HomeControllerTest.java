af50bb491e453f6738b08247a57e9e3083a33720
Balint <csernozjom1@gmail.com>
2019-10-25 15:27:08 +0200

    Format code and fix tests

--- src/test/java/tacos/HomeControllerTest.java
+++ src/test/java/tacos/HomeControllerTest.java
@@ -1,6 +1,6 @@
 package tacos;
 
-import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -10,8 +10,13 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+
+import tacos.data.TacoRepository;
+import tacos.data.IngredientRepository;
+import tacos.data.OrderRepository;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest
@@ -19,12 +24,20 @@ public class HomeControllerTest {
 
 	@Autowired
 	private MockMvc mockMvc;
-	
+
+	@MockBean
+	private IngredientRepository ingredientRepository;
+
+	@MockBean
+	private TacoRepository designRepository;
+
+	@MockBean
+	private OrderRepository orderRepository;
+
 	@Test
 	public void testHomePage() throws Exception {
-		mockMvc.perform(get("/"))
-		.andExpect(status().isOk())
-		.andExpect(view().name("home"))
-		.andExpect(content().string(containsString("Welcome to...")));
+		mockMvc.perform(get("/")).andExpect(status().isOk()).andExpect(view().name("home"))
+				.andExpect(content().string(containsString("Welcome to...")));
 	}
+
 }
