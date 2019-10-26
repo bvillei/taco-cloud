a564c9fd11a34f96762505aebd0cb1100306e648
Balint <csernozjom1@gmail.com>
2019-10-26 13:38:06 +0200

    Create In Memory Authentication

--- src/main/java/tacos/security/SecurityConfig.java
+++ src/main/java/tacos/security/SecurityConfig.java
@@ -1,11 +1,21 @@
 package tacos.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	// IN MEMORY AUTHENTICATION
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication()
+		.withUser("admin").password("{noop}a").roles("admin")
+		.and()
+		.withUser("user").password("{noop}b").roles("user");
 
+	}
 }
