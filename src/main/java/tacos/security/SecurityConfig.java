a7f815b334b63673457feba5ed4b8ad37b97ef20
Balint <csernozjom1@gmail.com>
2019-10-26 21:09:59 +0200

    Customizing user authentication

--- src/main/java/tacos/security/SecurityConfig.java
+++ src/main/java/tacos/security/SecurityConfig.java
@@ -1,21 +1,31 @@
 package tacos.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.StandardPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-	// IN MEMORY AUTHENTICATION
+	@Autowired
+	private UserDetailsService userDetailsService;
+
+	@Bean
+	public PasswordEncoder encoder() {
+		return new StandardPasswordEncoder("53cr3t");
+	}
+
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication()
-		.withUser("admin").password("{noop}a").roles("admin")
-		.and()
-		.withUser("user").password("{noop}b").roles("user");
+
+		auth.userDetailsService(userDetailsService).passwordEncoder(encoder());
 
 	}
+
 }
