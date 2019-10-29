0b93276175aec11ef46d4adb2acae7c53c883a6a
Balint <csernozjom1@gmail.com>
2019-10-29 11:25:50 +0100

    Using logged in user data when creating order

--- src/main/java/tacos/Order.java
+++ src/main/java/tacos/Order.java
@@ -10,6 +10,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
 import javax.persistence.PrePersist;
 import javax.persistence.Table;
 import javax.validation.constraints.Digits;
@@ -32,6 +33,9 @@ public class Order implements Serializable {
 	private Long id;
 
 	private Date placedAt;
+
+	@ManyToOne
+	private User user;
 
 	@NotBlank(message = "Delivery name is required")
 	private String deliveryName;
--- src/main/java/tacos/web/DesignTacoController.java
+++ src/main/java/tacos/web/DesignTacoController.java
@@ -1,5 +1,6 @@
 package tacos.web;
 
+import java.security.Principal;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -16,26 +17,33 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
+import lombok.extern.slf4j.Slf4j;
 import tacos.Ingredient;
 import tacos.Ingredient.Type;
 import tacos.Order;
 import tacos.Taco;
+import tacos.User;
 import tacos.data.IngredientRepository;
 import tacos.data.TacoRepository;
+import tacos.data.UserRepository;
 
 @Controller
 @RequestMapping("/design")
 @SessionAttributes("order")
+@Slf4j
 public class DesignTacoController {
 
 	private final IngredientRepository ingredientRepo;
 
 	private TacoRepository tacoRepo;
 
+	private UserRepository userRepo;
+
 	@Autowired
-	public DesignTacoController(IngredientRepository ingredientRepo, TacoRepository tacoRepo) {
+	public DesignTacoController(IngredientRepository ingredientRepo, TacoRepository tacoRepo, UserRepository userRepo) {
 		this.ingredientRepo = ingredientRepo;
 		this.tacoRepo = tacoRepo;
+		this.userRepo = userRepo;
 	}
 
 	@ModelAttribute(name = "order")
@@ -49,7 +57,8 @@ public class DesignTacoController {
 	}
 
 	@GetMapping
-	public String showDesignForm(Model model) {
+	public String showDesignForm(Model model, Principal principal) {
+		log.info("   --- Designing taco");
 		List<Ingredient> ingredients = new ArrayList<>();
 		ingredientRepo.findAll().forEach(i -> ingredients.add(i));
 
@@ -58,11 +67,17 @@ public class DesignTacoController {
 			model.addAttribute(type.toString().toLowerCase(), filterByType(ingredients, type));
 		}
 
+		String username = principal.getName();
+		User user = userRepo.findByUsername(username);
+		model.addAttribute("user", user);
+
 		return "design";
 	}
 
 	@PostMapping
 	public String processDesign(@Valid Taco taco, Errors errors, @ModelAttribute Order order) {
+
+		log.info("   --- Saving taco");
 
 		if (errors.hasErrors()) {
 			return "design";
